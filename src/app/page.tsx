8b0d4bef641ad6cbea32536e8b886f4907c479b4
Axel Verhaeghe <axel.verhaeghe@doccle.be>
2023-10-03 23:27:37 +0200

    Add nav background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,7 @@
-import PersonalInfo from '@/components/personal-info-section/personal-info';
 import styles from './page.module.scss';
-import { useMemo } from 'react';
-import SelectPlan from '@/components/select-plan-section/select-plan';
 import RegistrationForm from '@/components/registration-form/registration-form';
 
-export default function Home({
-  searchParams,
-}: {
-  params: { slug: string };
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
+export default function Home() {
   return (
     <main className={styles.main}>
       <RegistrationForm />
